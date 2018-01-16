eba5f7c3355f8553c2e1228daec559c1090baff3
John Siwicki <johnsiwicki@gmail.com>
2018-01-16 07:55:12 -0500

    Update errors.js

--- errors.js
+++ errors.js
@@ -1,24 +1,24 @@
  window.onerror = function(msg, url, linenumber) {
 
-	 // convert to string
-	 var nUrl = url.toString();
-	 var nLine = linenumber.toString();
-	 
-	 //airtable url get at https://airtable.com/api
-	 var airurl = "";
+     // convert to string
+     var nUrl = url.toString();
+     var nLine = linenumber.toString();
 
-	  //send data to airtable this is my airtable linkg
-		var airtable_write_endpoint = airurl;
-			axios.post(airtable_write_endpoint, {
-				"fields": {
-					"Error message": msg,
-					"URL": nUrl,
-					"Line" : nLine
-				}
-			}).then(function (response) {
-    console.log(response);
-  }).catch(function (error) {
-    console.log(error);
-  });
-	 	return true;
-}
+     //airtable url get at https://airtable.com/api
+     var airurl = "";
+
+     //send data to airtable this is my airtable linkg
+     var airtable_write_endpoint = airurl;
+     axios.post(airtable_write_endpoint, {
+         "fields": {
+             "Error message": msg,
+             "URL": nUrl,
+             "Line": nLine
+         }
+     }).then(function(response) {
+         console.log(response);
+     }).catch(function(error) {
+         console.log(error);
+     });
+     return true;
+ }
